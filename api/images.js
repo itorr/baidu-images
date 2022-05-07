005e7fb271a999ef7da93be5115444de25f2d135
itorr <itorrrrrr@me.com>
2022-05-07 12:36:56 +0800

    test

--- api/images.js
+++ api/images.js
@@ -1,5 +1,6 @@
 export default function handler(request, response) {
     response.status(200).json({
+        request,
       body: request.body,
       query: request.query,
       cookies: request.cookies,
